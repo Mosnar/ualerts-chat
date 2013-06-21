4ad968cae5b45d14f40cf7b6521dbc0438bbd4db
Brandon Foster <brandon.foster.vt@gmail.com>
2013-06-21 09:35:50 -0400

    Fix time when minute is less than 10.
    ACE-29

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatController.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatController.js
@@ -109,7 +109,13 @@ ChatController.prototype.handleMessageSubmit = function() {
 ChatController.prototype.onMessage = function(message) {
     var $chatbox = $('#chatbox');
     var date = new Date(message.messageDate);
-    var dateString = date.getHours() + ":" + date.getMinutes();
+    
+    var minutes = date.getMinutes();
+    if (minutes < 10) {
+    	minutes = "0" + minutes;
+    }
+
+    var dateString = date.getHours() + ":" + minutes;
     $chatbox.append('<p>' + '(' + dateString + ')' + ' ' +
         message.from + ': ' + message.text + '</p>');
     $chatbox.scrollTop($chatbox[0].scrollHeight);
