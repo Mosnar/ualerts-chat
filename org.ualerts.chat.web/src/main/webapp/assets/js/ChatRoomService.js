82b6c956fac7c4d445e1b9ef79989d18de2f2dc3
Brandon Foster <brandon.foster.vt@gmail.com>
2013-07-10 10:13:40 -0400

    Abstract large code blocks in ChatRoomService
    ACE-55

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomService.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomService.js
@@ -5,55 +5,46 @@ function ChatRoomService(remoteService) {
 }
 
 ChatRoomService.prototype.onMessage = function(message) {
-  if (message.type != "chat" || this.chatRoomList.length == 0) {
-    return;
-  }
-  
-  var chatRoomName = "";
-  
-  // to me, from <User1>
-//  if (message.from == this.username) {
-//	  chatRoomName = message.to;
-//  }
-//  if ((message.to == this.username) && (message.from != this.username)) {
-//	  chatRoomName == message.to;
-//  }
-//  if (message.to == "all") {
-//	  chatRoomName = message.to;
-//  }
-  
-  switch(message.to) {
-  case this.username:	// to me, from <User1>
-	  chatRoomName = message.from;
-	  break;
-  case "all":
-	  chatRoomName = "all";
-	  console.log('case "all" executed');
-	  break;
-  default:	// to <User1>, from me
-	  chatRoomName = message.to;
-  }
-  
-  var room = this.getChatRoom(chatRoomName);
-  if (room == false) {
-	  room = this.createChatRoom(chatRoomName);
-	  console.log('this.createChatRoom(chatRoomName) is: ' + room);
-	  console.log('message.to is: ' + message.to);
-  }
-  room.displayChatMessage(message);
-  console.log('sending a messge to ' + message.to + ', from ' + message.from);
+	var chatRoomName = "";
+	var room = "";
+	
+	if (message.type != "chat" || this.chatRoomList.length == 0) {
+		return;
+	}
+	
+	switch(message.to) {
+	case this.username: // to me, from <User1>
+		chatRoomName = message.from;
+		break;
+	case "all":
+		chatRoomName = "all";
+		break;
+	default:
+		chatRoomName = message.to;
+	}
+	
+	room = this.getChatRoom(chatRoomName);
+	if (room == false) {
+		room = this.createChatRoom(chatRoomName);
+	}
+	room.displayChatMessage(message);
 };
 
 ChatRoomService.prototype.setUsername = function(username) {
-  this.username = username;
+	this.username = username;
 };
 
 ChatRoomService.prototype.createChatRoom = function(chatRoomName) {
 	var room = new ChatRoom(chatRoomName, this.username, this.remoteService);
-	this.chatRoomList.push(room);
-	if (chatRoomName != "all") {
-		$(".chat-holder").append(room.$uiDom);
+	
+	function displayChatRoom() {
+		if (chatRoomName != "all") {
+			$(".chat-holder").append(room.$uiDom);
+		}
 	}
+	
+	this.chatRoomList.push(room);
+	displayChatRoom();
 	return room;
 };
 
