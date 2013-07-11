a13e1ed26db477732442fd204cc28be6019c3656
Ransom Roberson <ransom93@vt.edu>
2013-07-11 14:32:42 -0400

    Corrected small error in parseUrls regex

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoom.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoom.js
@@ -62,17 +62,22 @@ function ChatRoom(chatRoomName, username, remoteService) {
 	}
 }
 
+/**
+ * Parse a block of text to detect URLs and convert them to hyperlinks
+ * @param text string to parse
+ * @return text with converted links
+ */
 function parseUrls(text) {
 	var replacedText, replacePattern1;
-	 
+	
 	//URLs starting with http://, https://, ftps://, ftp://
-	replacePattern1 = /(\b(https?|ftps?):\/\/[-A-Z0-9+&amp;@#\/%?=~_|!:,.;]*[-A-Z0-9+&amp;@#\/%=~_|])/ig;
+	replacePattern1 = /(\b(https?|ftps?):\/\/[-A-z0-9+&amp;@#\/%?=~_|!:,.;]*[-A-Z0-9+&amp;@#\/%=~_|])/ig;
 	replacedText = text.replace(replacePattern1, '<a title="$1" href="$1" target="_blank">$1</a>');
 	 
 	//URLs starting with "www."
 	replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 	replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-return replacedText;
+	return replacedText;
 }
 
 /**
