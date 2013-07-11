78685a6c018ff837966048f793f4641515999083
Brandon Foster <brandon.foster.vt@gmail.com>
2013-07-11 15:05:30 -0400

    Create ChatRoomManager. ChatRoomService delegates to ChatRoomManager.
    ACE-60

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomService.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomService.js
@@ -1,23 +1,14 @@
 function ChatRoomService(remoteService) {
   this.username = null;
-  this.chatRoomList = new Array();
   this.remoteService = remoteService;
+  this.chatRoomManager = new ChatRoomManager();
 }
-
-ChatRoomService.prototype.removeChatRoomByName = function(name) {
-	for (var i = 0; i < this.chatRoomList.length; i++) {
-		if (this.chatRoomList[i].name == name) {
-			this.chatRoomList.splice(i, 1);
-			return;
-		}
-	}
-};
 
 ChatRoomService.prototype.onMessage = function(message) {
 	var chatRoomName = "";
 	var room = "";
 	
-	if (message.type != "chat" || this.chatRoomList.length == 0) {
+	if (message.type != "chat" || this.chatRoomManager.chatRoomList.length == 0) {
 		return;
 	}
 	
@@ -44,25 +35,14 @@ ChatRoomService.prototype.setUsername = function(username) {
 };
 
 ChatRoomService.prototype.createChatRoom = function(chatRoomName) {
-	var room = new ChatRoom(chatRoomName, this.username, this.remoteService);
-	
-	function displayChatRoom() {
-		if (chatRoomName != "all") {
-			$(".chat-holder").append(room.$uiDom);
-		}
-	}
-	
-	this.chatRoomList.push(room);
-	displayChatRoom();
-	return room;
+	return this.chatRoomManager.addChatRoom(chatRoomName, this.username, this.remoteService);
+};
+
+
+ChatRoomService.prototype.removeChatRoomByName = function(name) {
+	this.chatRoomManager.removeChatRoom(name);
 };
 
 ChatRoomService.prototype.getChatRoom = function(chatRoomName) {
-	var list = this.chatRoomList;
-	for (var i = 0; i < list.length; i++) {
-		if (list[i].name == chatRoomName) {
-			return list[i];
-		};
-	}
-	return false;
+	return this.chatRoomManager.getChatRoom(chatRoomName);
 };
