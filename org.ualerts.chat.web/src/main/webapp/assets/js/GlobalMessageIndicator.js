32c5d6bda0c9ed3c8d779b6f16a33f2b8d5d8e18
Ransom Roberson <ransom93@vt.edu>
2013-07-15 10:22:50 -0400

    Switched from timeout timer to interval timer Added docs

--- org.ualerts.chat.web/src/main/webapp/assets/js/GlobalMessageIndicator.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/GlobalMessageIndicator.js
@@ -4,45 +4,65 @@ function GlobalMessageIndicator(remoteService) {
 	var timer = null;
 	var windowFocus = true;
 	var soundFile = CONTEXT_PATH + '/assets/sounds/notification';
+	var flashState = true;
 	
+	/**
+	 * Called automatically on message receive. Checks to see if notifications
+	 * are necessary.
+	 * @param message message object received
+	 */
 	GlobalMessageIndicator.prototype.onMessage = function(message) {
-		if (message.type != "chat" || windowFocus == false) {
+		if (message.type != "chat" || windowFocus == true) {
 			return;
 		}
 		numMessages++;
-		clearTimeout(timer);
-		startTitlePulse(numMessages);
+		clearInterval(timer);
+		startTitlePulse();
 		playSound(soundFile);
 	};
 	
+	// On page focus
 	$(window).focus(function() {
 		windowFocus = true;
 		stopTitlePulse();
 	})
+	// On page blur
 	.blur(function() {
-			windowFocus = false;
+		windowFocus = false;
 	});
 	
-	function startTitlePulse(numMessages) {
-		var msgGrammar = "s";
-		if (numMessages == 1) {
-			msgGrammar = "";
-		}
-		
-		document.title = "(" + numMessages + " unread message"+ msgGrammar +") " + originalTitle;
-		timer = setTimeout(function(){resetTitlePulse(numMessages);}, 1200);
+	/**
+	 * Sets a timer to call the title notification pulsing until focus
+	 */
+	function startTitlePulse() {
+		timer = setInterval(function(){doPulse();}, 1200);
 	}
 
-	function resetTitlePulse(num) {
-		document.title = originalTitle;
-		timer = setTimeout(function(){startTitlePulse(numMessages);}, 1200);	
-	};
+	/**
+	 * Begins flashing the page title with notifications of messages
+	 */
+	function doPulse() {
+		if (flashState) {
+			document.title = originalTitle;
+		} else {
+			var msgGrammar = "s";
+			if (numMessages == 1) {
+				msgGrammar = "";
+			}
+			
+			document.title = "(" + numMessages + " unread message"+ msgGrammar +") " + originalTitle;
+		}
+		flashState = !flashState;
+	}
 	
+	/**
+	 * Stops the title notifications
+	 */
 	function stopTitlePulse() {
 		document.title = originalTitle;
-		clearTimeout(timer);
+		clearInterval(timer);
 		numMessages = 0;
-	};
+	}
 	
 	/**
 	 * Play a sound file in the browser using HTML5 audio tags or fallback to
@@ -51,8 +71,7 @@ function GlobalMessageIndicator(remoteService) {
 	 * 	of the same name.
 	 */
 	function playSound(fileName) {   
-		console.log('Playing: ' + fileName);
         document.getElementById("sound").innerHTML='<audio autoplay="autoplay"><source src="' + fileName + '.mp3" type="audio/mpeg" /><source src="' + fileName + '.ogg" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + fileName +'.mp3" /></audio>';
-    };
+    }
 
 }
