5472f3c3a5000bed159efdba77aa133f3bdfa6b1
Brandon Foster <brandon.foster.vt@gmail.com>
2013-07-16 11:52:56 -0400

    Temporarily remove redraw method to merge in Billy's code
    ACE-62

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomManager.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoomManager.js
@@ -28,59 +28,22 @@ ChatRoomManager.prototype.getChatRoom = function(chatRoomName) {
 	return false;
 };
 
-ChatRoomManager.prototype.redraw = function() {
 
-	var windowWidth = $(window).width();
-	console.log('windowWidth: ' + windowWidth);
-
-	var chatRoomWidth = $('.chatroom-container:visible').last().width();
-	console.log('chatRoomWidth: ' + chatRoomWidth);
-	var count = 0;
-	var maxNum = windowWidth/chatRoomWidth;
-	console.log('maxNum: ' + maxNum);
-
-	for (var i = this.chatRoomList.length - 1; i >= 1; i--) {		
-		if(count < maxNum) {
-			this.chatRoomList[i].$uiDom.find('.chatroom-container').show();
-			console.log('show');
-			count++;
-			console.log('count: ' + count);
-		}
-		else {
-			this.chatRoomList[i].$uiDom.find('.chatroom-container').hide();
-			console.log('this.chatRoomList: ' + this.chatRoomList);
-			console.log(this.chatRoomList[i].$uiDom.find('.chatroom-container'));
-			console.log('hide');
-		}
-	}
-
-//	var chatRoomWidth = $('.chatroom-container:visible').last().width();
-//	var numChatRooms = $('.chatroom-container').length;
-//	var sumChatRoomWidth = chatRoomWidth * (numChatRooms + 1 );
-//	console.log('sumChatRoomWidth: ' + sumChatRoomWidth);
-//	console.log('$(window).width(): ' + $(window).width());
-//	if ($(window).width() < sumChatRoomWidth) {
-//		console.log('if reached');
-//		$('.chatroom-container:visible').first().hide();
-//	}
-};
 
 ChatRoomManager.prototype.focusOnChatRoom = function(chatRoomName) {
-
-        //find this chatRoom in the array
-        var selectedIndex = null;
-        for (var i = 0; i < this.chatRoomList.length; i++) {
-                if (this.chatRoomList[i].name == chatRoomName) {
-                        selectedIndex = i;
-                        break;
-                }
-        }
-        
-        $('.chatroom-container:contains('+chatRoomName+')').insertAfter($('.chatroom-container').last());
-
-
-        //set this chatRoom's array position to be last
-        this.chatRoomList.move(selectedIndex, this.chatRoomList.length-1);
+	//find this chatRoom in the array
+	var selectedIndex = null;
+	for (var i = 0; i < this.chatRoomList.length; i++) {
+		if (this.chatRoomList[i].name == chatRoomName) {
+			selectedIndex = i;
+			break;
+		}
+	}
+	
+	$('.chatroom-container:contains(' + chatRoomName + ')').insertAfter($('.chatroom-container').last());
+	
+	//set this chatRoom's array position to be last
+	this.chatRoomList.move(selectedIndex, this.chatRoomList.length - 1);
 };
 
 Array.prototype.move = function (from, to) {
