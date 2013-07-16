6d5edf00f35c66537f03b75c8bf472a5e60f7492
Brandon Foster <brandon.foster.vt@gmail.com>
2013-07-16 14:41:29 -0400

    Fix bug: non-right-most chatroom windows can now send messages.
    ACE-62

--- org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoom.js
+++ org.ualerts.chat.web/src/main/webapp/assets/js/ChatRoom.js
@@ -3,6 +3,7 @@ function ChatRoom(chatRoomName, username, remoteService) {
 	this.username = username;
 	this.remoteService = remoteService;
 	this.$uiDom = "";
+	this.$chatRoomMessageField = null;
 	
 	var self = this;
 	
@@ -37,18 +38,15 @@ function ChatRoom(chatRoomName, username, remoteService) {
 	}
 	
 	function onMessageSend(self) {
-	    $chatRoomMessageField = self.$uiDom.find('.chatRoomMessageField');
+	    self.$chatRoomMessageField = self.$uiDom.find('.chatRoomMessageField');
 	    self.$uiDom.find('.chatroomMessageButton').click(function() {
-	    	if ($.trim($chatRoomMessageField.val()) != "") {
-	    		var clientMessage = $chatRoomMessageField.val();
+	    	if ($.trim(self.$chatRoomMessageField.val()) != "") {
+	    		var clientMessage = self.$chatRoomMessageField.val();
 	    		self.remoteService.sendMessage(self.username, self.name, "chat", clientMessage);
-	    		$chatRoomMessageField.val('');
+	    		self.$chatRoomMessageField.val('');
 	    	}
 	    });
 	}
-	
-
-	
 	
 	if (chatRoomName == 'all') {
 		this.$uiDom = $('<div id="chatbox"></div>');
